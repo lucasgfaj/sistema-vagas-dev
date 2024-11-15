44f0fa94dac425ca1f03de2519dde42dedcf1b79
lucasgfaj <lucasgfajardo09@gmail.com>
2024-11-15 15:28:56 -0300

    Add: userConstructor

--- src/models/User.ts
+++ src/models/User.ts
@@ -8,74 +8,42 @@ export interface UserDependencies {
 
 export default class User {
 
-    private id!: number;
-    protected name!: string;
-    private email!: string;
-    private password!: string;
-    private typeUser!: "desenvolvedor" | "empresa";
-    private dateCreate!: Date;
+    private userDependencies: UserDependencies;
 
-    public constructor(userDependencies: UserDependencies) {
-        this.id = userDependencies.id;
-        this.name = userDependencies.name;
-        this.email = userDependencies.email;
-        this.password = userDependencies.password;
-        this.typeUser = userDependencies.typeUser;
-        this.dateCreate = new Date();
+    public constructor(userDependencies:UserDependencies) {
+        this.userDependencies = userDependencies;
     }
 
     public getID(): number {
-        return this.id
+        return this.userDependencies.id
     }
 
     public setID(id: number): void {
-        this.id = id
+        this.userDependencies.id = id
     }
 
     public getName(): string {
-        return this.name;
+        return this.userDependencies.name;
     }
 
     public setName(name: string): void {
-        this.name = name
+        this.userDependencies.name = name
     }
 
     public getEmail(): string {
-        return this.email;
+        return this.userDependencies.email;
     }
 
     public setEmail(email: string): void {
-        this.email = email
+        this.userDependencies.email = email
     }
 
     public getPassword(): string {
-        return this.password;
+        return this.userDependencies.password;
     }
 
     public setPassword(password: string): void {
-        this.password = password
+        this.userDependencies.password = password
     }
-
-
-    public validateEmail(): boolean {
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return regex.test(this.email);
-    }
-
-    public getUserInfo(): string {
-        return `ID: ${this.id}, Name: ${this.name}, Email: ${this.email}, Type: ${this.typeUser}, Date Created: ${this.dateCreate}`;
-    }
-
-    // Método da Classe de Alterar Senha do Usuário//
-   /* public alterPassword(newPassword: string): void{
-        if (this.newPassword.length >= 6){
-            this.password = newPassword;
-            console.log("Alter Password Sucess!")
-         } else {
-            console.log ("This password is not validate, password has 6 caracters!")
-         }
-        
-    }
-    */
 }
 
